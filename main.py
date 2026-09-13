c1b57d8a5acba1d1e1a0433524f469ff40c0bad5
grf <tuprofe101@gmail.com>
2026-09-13 11:20:27 +0200

    V001

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.installer import Installer
+
+
+def install():
+    installer = Installer()
+    installer.install()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    install()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
